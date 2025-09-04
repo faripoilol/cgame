0d716cda3c7496cdd61f0bb81c9027bfd2a9d9cb
Vlad <faripoilol260@gmail.com>
2025-09-04 21:12:11 +0400

    First differences

--- Game00/main.cpp
+++ Game00/main.cpp
@@ -1,5 +1,6 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+#include <math.h>
 
 LRESULT CALLBACK WndProc(HWND h, UINT m, WPARAM w, LPARAM l) {
     if (m == WM_DESTROY) { PostQuitMessage(0); return 0; }
@@ -18,7 +19,7 @@ int APIENTRY wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, int) {
 
     HWND wnd = CreateWindowEx(
         0, cls, L"2D Starter", WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 960, 540,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 640,
         nullptr, nullptr, hInst, nullptr);
     ShowWindow(wnd, SW_SHOW);
 
@@ -29,6 +30,7 @@ int APIENTRY wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, int) {
 
     // player
     float x = 100.f, y = 100.f;
+	float phi = 0.f; // angle
     const int w = 64, h = 64;
 
     MSG msg{};
@@ -48,8 +50,8 @@ int APIENTRY wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, int) {
 
         // input
         float vx = 0.f, vy = 0.f;
-        if (GetAsyncKeyState(VK_RIGHT) & 0x8000) vx += 300.f;
-        if (GetAsyncKeyState(VK_LEFT) & 0x8000) vx -= 300.f;
+        if (GetAsyncKeyState(VK_RIGHT) & 0x8000) { vx += 300.f; phi -= 0.01; }
+		if (GetAsyncKeyState(VK_LEFT) & 0x8000) { vx -= 300.f; phi += 0.01; }
         if (GetAsyncKeyState(VK_DOWN) & 0x8000) vy += 300.f;
         if (GetAsyncKeyState(VK_UP) & 0x8000) vy -= 300.f;
 
@@ -60,6 +62,7 @@ int APIENTRY wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, int) {
         if (x < 0) x = 0; if (y < 0) y = 0;
         if (x > rc.right - w)  x = float(rc.right - w);
         if (y > rc.bottom - h) y = float(rc.bottom - h);
+		if (phi < 0) phi = 0; if (phi > 3.14f) phi = 3.14f;
 
         // draw (simple double buffer)
         HDC hdc = GetDC(wnd);
@@ -70,9 +73,23 @@ int APIENTRY wWinMain(HINSTANCE hInst, HINSTANCE, LPWSTR, int) {
         HBRUSH bg = CreateSolidBrush(RGB(30, 30, 40));
         FillRect(mdc, &rc, bg); DeleteObject(bg);
 
+        // draw a line to indicate the angle
+        HPEN pen = CreatePen(PS_SOLID, 30, RGB(255, 255, 255)); // 10px, white
+        HPEN oldPen = (HPEN)SelectObject(mdc, pen);
+        MoveToEx(mdc, 400, 600, nullptr);
+        LineTo(mdc, 400 + (int)(150 * cos(phi)), 600 - (int)(150 * sin(phi)));
+        SelectObject(mdc, oldPen);
+        DeleteObject(pen);
+
         RECT r{ (int)x, (int)y, (int)x + w, (int)y + h };
         HBRUSH br = CreateSolidBrush(RGB(250, 170, 30));
-        FillRect(mdc, &r, br); DeleteObject(br);
+        // FillRect(mdc, &r, br); 
+		HBRUSH oldBr = (HBRUSH)SelectObject(mdc, br);
+        Ellipse(mdc, 300, 500, 500, 700);
+		
+		SelectObject(mdc, oldBr);
+		DeleteObject(oldBr);
+        DeleteObject(br);
 
         BitBlt(hdc, 0, 0, rc.right, rc.bottom, mdc, 0, 0, SRCCOPY);
         SelectObject(mdc, oldBmp);
